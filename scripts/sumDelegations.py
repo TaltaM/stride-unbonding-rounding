e694fc88a55945b135deca713db5362eb1ba26d3
Talta <68383301+TaltaM@users.noreply.github.com>
2022-12-14 15:56:46 +0100

    Update sum script, add first osmosis reponse log

--- scripts/sumDelegations.py
+++ scripts/sumDelegations.py
@@ -1,9 +1,19 @@
+#!/usr/bin/python3
+
 import json, sys
 
-data = json.load(sys.stdin)
+def main(args):
+    try:
+        inputFile = open(args[1])
+        data = json.load(inputFile)
+        inputFile.close()
+    except IndexError:
+        return False
+    amount = 0
+    for i in data['delegation_responses']:
+        amount += int(i['balance']['amount'])
+    print(amount)
+    return True
 
-amount = 0
-for i in data['delegationResponses']:
-	amount += int(i['balance']['amount'])
-
-print(amount)
+if __name__ == "__main__":
+    sys.exit(not main(sys.argv))
