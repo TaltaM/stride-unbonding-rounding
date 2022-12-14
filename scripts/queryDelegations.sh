ed0a03f0b9bc7a09ed89970fb841a43c2b74a973
Talta <68383301+TaltaM@users.noreply.github.com>
2022-12-14 16:01:44 +0100

    Update sum script

--- scripts/queryDelegations.sh
+++ scripts/queryDelegations.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-grpcurl -plaintext -H "x-cosmos-block-height: $1"  -d '{"delegator_addr": "$2"}' $3 cosmos.staking.v1beta1.Query/DelegatorDelegations > $4 
+
+# Usage:
+# Param 1: API URL
+# Param 2: Delegator address
+# Param 3: Block height
+# Param 4: Output file
+
+# COSMOS API:
+# JUNO API:
+# OSMOSIS API: https://osmo-fleet-api.main.stridenet.co/
+# STARGAZE API:
+
+curl -X -H "x-cosmos-block-height: $3" $1/cosmos/staking/v1beta1/delegations/$2 -o $4
